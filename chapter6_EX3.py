ca45a94a12457e1ae48cf7ebbcdfdb14b17def43
kun1987 <56306711+kun1987@users.noreply.github.com>
2019-10-10 02:28:45 +0200

    Update chapter6_EX3.py

--- chapter6_EX3.py
+++ chapter6_EX3.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
-"""
-Created on Thu Oct 10 02:15:53 2019
-
-@author: fengtian
-"""
 
 def is_between(x,y,z):
     #this is a function is_between(x, y, z) that returns True if x ≤ y ≤ z or False otherwise.
