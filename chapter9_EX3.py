4759c24ee80cb8ce98205d9ab0ba7a6c6c453ce9
kun1987 <56306711+kun1987@users.noreply.github.com>
2019-10-12 20:11:04 +0200

    Add files via upload

---
+++ chapter9_EX3.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+fin = open('words.txt')
+
+def avoid(word,letters):
+    #this function takes a word and a string of forbidden letters, 
+    #and that returns True if the word doesn’t use any of the forbidden letters.
+    if letters not in word:
+        return True
+    else:
+        return False
+    
+def avoid_costom():
+    #prompt the user to enter a string of forbidden letters 
+    letters = raw_input('Enter the forbidden letters: ')
+    return letters
+
+def count(fin, letters):
+    #print the persent of words that don’t contain any of forbidden letters.
+    count2 = 0
+    count1 = 0
+    for line in fin:
+       if avoid(line,letters) == True:
+            print line
+            count2 += 1
+       else:
+            count1 += 1
+    percent = count2*1.0/(count1+count2)*100
+    return percent
+        
+if __name__ == "__main__":
+    print str(count(fin,avoid_costom())) + "% of the forbidden words"
