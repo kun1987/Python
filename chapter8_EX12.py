d22418351fe0fe7ea1e4eae2189199dcf41f1fd1
kun1987 <56306711+kun1987@users.noreply.github.com>
2019-10-12 17:02:27 +0200

    Add files via upload

---
+++ chapter8_EX12.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+#from https://greenteapress.com/thinkpython/code/rotate.py
+
+def rotate_letter(letter, n):
+    """Rotates a letter by n places. 
+    letter: single-letter string
+    n: int
+    Returns: new single-letter string
+    """
+    if letter.isupper():
+        start = ord('A')
+    elif letter.islower():
+        start = ord('a')
+    else:
+        return letter
+
+    c = ord(letter) - start
+    i = (c + n) % 26 + start
+    return chr(i)
+
+
+def rotate_word(word, n):
+    """Rotates a word by n places.
+
+    word: string
+    n: integer
+
+    Returns: string
+    """
+    res = ''
+    for letter in word:
+        res += rotate_letter(letter, n)
+    return res
+
+
+if __name__ == '__main__':
+    print rotate_word('cheer', 7)
+    print rotate_word('melon', -10)
+    print rotate_word('sleep', 9)
