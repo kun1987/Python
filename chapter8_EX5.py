5e35ea84fd2921771b27f2152db9e000bc38a122
kun1987 <56306711+kun1987@users.noreply.github.com>
2019-10-12 12:30:04 +0200

    Update chapter8_EX5.py

--- chapter8_EX5.py
+++ chapter8_EX5.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
-"""
-Created on Sat Oct 12 12:20:12 2019
-
-@author: fengtian
-"""
 
 def count(word,counting_letter):
     #this function counts the number of times a given letter appears in a word
